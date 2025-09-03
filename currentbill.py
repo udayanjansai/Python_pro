5b55270a2b9c72214c486140794e3ae6e4ee66a5
udayanjansai <udayanjansai@gmail.com>
2025-09-03 15:20:12 +0530

    Delete currentbill.py

--- currentbill.py
+++
@@ -1,8 +0,0 @@
-cuno=int(input("Enter customer number: "))
-cname=input("Enter customer name: ")
-p,l=map(int,input("Enter present and last month bill ").split())
-cpu=float(input("Enter cost per unit: "))
-totunits=p-l
-bill=totunits*cpu
-print("Customer no:", cuno, "name:", cname, "present bill:", p, "last month bill:", l)
-print("total units:",totunits,"total bill:",bill)

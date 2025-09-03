82599dc26fb4c6a2687059657aac9ac813ea24c4
udayanjansai <udayanjansai@gmail.com>
2025-09-03 15:23:26 +0530

    Delete vowels.py

--- vowels.py
+++
@@ -1,8 +0,0 @@
-vowels=['a','e','i','o','u']
-def vowel(s):
-    if s in vowels or s in [v.upper() for v in vowels]:
-        print("vowels")
-    else:
-        print("consonant")
-c=input("Enter a character: ")
-vowel(c)

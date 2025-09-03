17a4de4708caaa7d71fd582d9e8f31fca34ed6c5
udayanjansai <udayanjansai@gmail.com>
2025-09-03 15:22:51 +0530

    Delete simpleintereat.py

--- simpleintereat.py
+++
@@ -1,9 +0,0 @@
-p,t,r=map(int,input().split())
-print("simple interest:",(p*t*r)/100)
-def interest(p,t,r):
-    i=(p*r)/100
-    c=0
-    while c<t:
-        p+=i
-        c+=1
-    print("Compound interest:",p)

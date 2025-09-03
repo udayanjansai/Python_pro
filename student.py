28099bae66bc055f3244363c534c576642367a3d
udayanjansai <udayanjansai@gmail.com>
2025-09-03 15:23:00 +0530

    Delete student.py

--- student.py
+++
@@ -1,7 +0,0 @@
-stuno=int(input("Enter student number: "))
-stuname=input("Enter student name: ")
-m1,m2,m3=map(int,input().split())
-tot=m1+m2+m3
-avg=round(tot/3,2)
-print("student no",stuno,"name:",stuname,"marks:",m1,m2,m3)
-print("total:",tot,"avg:",avg)

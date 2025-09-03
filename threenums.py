7eafb70a5ff63e95f616fac9680108baef5cd8d1
udayanjansai <udayanjansai@gmail.com>
2025-09-03 15:23:17 +0530

    Delete threenums.py

--- threenums.py
+++
@@ -1,9 +0,0 @@
-def three(a,b,c):
-    if (a>b) and (a>c):
-        print(a,"is the largest")
-    elif (b>a) and (b>c):
-        print(b,"is the Largest")
-    else:
-        print(c,"is the Largest")
-a,b,c=map(int,input().split())
-three(a,b,c)    
